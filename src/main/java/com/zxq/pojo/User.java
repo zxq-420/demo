4f41fafce81c8eef365cc8cc3e5d93d548853638
zxq <18339166725@163.com>
2020-08-04 19:53:45 +0800

    update toString

--- src/main/java/com/zxq/pojo/User.java
+++ src/main/java/com/zxq/pojo/User.java
@@ -3,4 +3,12 @@ package com.zxq.pojo;
 public class User {
     private String username;
     private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
